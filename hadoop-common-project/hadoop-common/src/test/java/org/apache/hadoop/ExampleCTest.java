e187fdae0d06a1b4e509c09c476ede8327d368bd
shuaiwang516 <shuaiwang516@gmail.com>
2023-10-26 21:01:16 -0500

    add an example of ctest

---
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ExampleCTest.java
@@ -0,0 +1,45 @@
+package org.apache.hadoop;
+
+import edu.illinois.ConfigTest;
+import edu.illinois.ConfigTestClass;
+import edu.illinois.ConfigTestRunner;
+import edu.illinois.UnUsedConfigParamException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+/**
+ * Author: Shuai Wang
+ * Date:  10/26/23
+ */
+@RunWith(ConfigTestRunner.class)
+@ConfigTestClass(value={"class-parameter1"}, file="src/test/resources/ExampleCTest.json")
+public class ExampleCTest {
+    private Configuration conf = null;
+    @Before
+    public void setUp() {
+        conf = new Configuration();
+    }
+
+    @ConfigTest()
+    public void configTestShouldPass() {
+        System.out.println("In test configTestShouldPass");
+        // These two parameters are "required" in the @ConfigTestClass
+        conf.get("file-parameter1");
+        conf.get("file-parameter2");
+        conf.get("class-parameter1");
+    }
+
+    /**
+     * The test should throw an exception because the parameter "method-parameter2" is not used in the test.
+     */
+    @ConfigTest(value={"method-parameter2"}, expected = UnUsedConfigParamException.class)
+    public void configTestShouldFail() {
+        System.out.println("In test configTestShouldFail");
+        // These two parameters are "required" in the @ConfigTestClass
+        conf.get("file-parameter1");
+        conf.get("file-parameter2");
+        conf.get("class-parameter1");
+    }
+}
